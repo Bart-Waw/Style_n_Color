215b98d3aa3ce90aea77e254c02710c39c2de145
Bart-Waw <wawrejkob@gmail.com>
2021-05-10 15:32:45 +0100

    testing colors - rigid

--- scripting.js
+++ scripting.js
@@ -66,11 +66,155 @@ $('#page_4_primary').on('click', function () {
 });
 */
 
+/*
 $('#slider').on('change', function () {
-    $('#shirt').css('fill', `hsl(${$('#slider').val()}, 80%, 50%)`);
+    $('#shirt').css('fill', `hsl(${$('#slider').val()}, 80%, 50%)`); 
 });
-
 
 $('#page_4_secondary').on('click', function () {
     $('#pants').css('fill', `hsl(${$('#slider').val()}, ${Math.floor(Math.random() * 101)}%, ${Math.floor(Math.random() * 101)}%)`);
+});
+*/
+
+$('#page_4_primary').on('click', function () {
+    $('#page_4_main_color_choice').slideToggle();
+});
+
+$('.colorBoxMain').on('click', function () {
+    $('#shirt').css('fill', $(this).css('background-color'));
+    $('.colorBoxSecondary').show();
+    if ($('#shirt').css('fill') !== $('#khaki').css('background-color')) {
+        $('#khaki').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#olive').css('background-color')) {
+        $('#olive').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#burgundy').css('background-color') 
+    && $('#shirt').css('fill') !== $('#green').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_green').css('background-color')
+    && $('#shirt').css('fill') !== $('#olive').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_brown').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_gray').css('background-color')
+    && $('#shirt').css('fill') !== $('#khaki').css('background-color')
+    ) {
+        $('#burgundy').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#beige').css('background-color')) {
+        $('#maroon').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#beige').css('background-color') 
+    && $('#shirt').css('fill') !== $('#navy').css('background-color')
+    && $('#shirt').css('fill') !== $('#light_pink').css('background-color')
+    && $('#shirt').css('fill') !== $('#white').css('background-color')
+    ) {
+        $('#beige').hide();
+    }
+    if ($('#shirt').css('fill') === $('#tan').css('background-color') 
+    || $('#shirt').css('fill') === $('#camel').css('background-color')
+    || $('#shirt').css('fill') === $('#light_pink').css('background-color')
+    || $('#shirt').css('fill') === $('#black').css('background-color')
+    || $('#shirt').css('fill') === $('#light_gray').css('background-color')
+    || $('#shirt').css('fill') === $('#dark_gray').css('background-color')
+    ) {
+        $('#navy').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#dark_green').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_brown').css('background-color')
+    ) {
+        $('#dark_green').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#burgundy').css('background-color') 
+    && $('#shirt').css('fill') !== $('#green').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_green').css('background-color')
+    && $('#shirt').css('fill') !== $('#olive').css('background-color')
+    && $('#shirt').css('fill') !== $('#khaki').css('background-color')
+    && $('#shirt').css('fill') !== $('#beige').css('background-color')
+    ) {
+        $('#brown').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#navy').css('background-color') 
+    && $('#shirt').css('fill') !== $('#light_pink').css('background-color')
+    && $('#shirt').css('fill') !== $('#black').css('background-color')
+    && $('#shirt').css('fill') !== $('#light_gray').css('background-color')
+    ) {
+        $('#light_pink').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#pink').css('background-color') 
+    ) {
+        $('#pink').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#beige').css('background-color') 
+    && $('#shirt').css('fill') !== $('#tan').css('background-color')
+    && $('#shirt').css('fill') !== $('#vamel').css('background-color')
+    && $('#shirt').css('fill') !== $('#light_pink').css('background-color')
+    && $('#shirt').css('fill') !== $('#white').css('background-color')
+    ) {
+        $('#tan').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#navy').css('background-color') 
+    && $('#shirt').css('fill') !== $('#beige').css('background-color')
+    && $('#shirt').css('fill') !== $('#tan').css('background-color')
+    && $('#shirt').css('fill') !== $('#camel').css('background-color')
+    && $('#shirt').css('fill') !== $('#light_pink').css('background-color') 
+    && $('#shirt').css('fill') !== $('#black').css('background-color')
+    && $('#shirt').css('fill') !== $('#light_gray').css('background-color')
+    && $('#shirt').css('fill') !== $('#white').css('background-color')
+    ) {
+        $('#light_blue').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#burgundy').css('background-color')
+    && $('#shirt').css('fill') !== $('#light_pink').css('background-color')
+    && $('#shirt').css('fill') !== $('#light_gray').css('background-color')
+    ) {
+        $('#light_gray').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#burgundy').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_gray').css('background-color')
+    && $('#shirt').css('fill') !== $('#pink').css('background-color')
+    ) {
+        $('#dark_gray').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#tan').css('background-color')
+    && $('#shirt').css('fill') !== $('#camel').css('background-color')
+    ) {
+        $('#cream').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#camel').css('background-color')
+    ) {
+        $('#camel').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#dark_brown').css('background-color')
+    ) {
+        $('#dark_brown').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#green').css('background-color')
+    ) {
+        $('#green').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#red').css('background-color')
+    ) {
+        $('#red').hide();
+    }
+    if ($('#shirt').css('fill') === $('#pink').css('background-color')
+    ) {
+        $('#white').hide();
+    }
+    if ($('#shirt').css('fill') !== $('#burgundy').css('background-color') 
+    && $('#shirt').css('fill') !== $('#red').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_brown').css('background-color')
+    && $('#shirt').css('fill') !== $('#pink').css('background-color')
+    && $('#shirt').css('fill') !== $('#black').css('background-color')
+    && $('#shirt').css('fill') !== $('#dark_gray').css('background-color')
+    && $('#shirt').css('fill') !== $('#white').css('background-color')
+    ) {
+        $('#black').hide();
+    }
+});
+
+$('#page_4_secondary').on('click', function () {
+    $('#page_4_secondary_color_choice').slideToggle();
+});
+
+$('.colorBoxSecondary').on('click', function () {
+    $('#pants').css('fill', $(this).css('background-color'));
 });
